ae2ee2af4454f9e1264a6f2ffadf16e3884278d3
Aayush Shah <aayushshah2597@gmail.com>
2022-03-31 15:29:53 -0600

    Sidebar functionality added and finished

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import "./App.css";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 import Home from "./Components/Home";
+import History from "./Components/History";
+import Profile from "./Components/Profile";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
       <div className="main-content">
-        <Sidebar />
-        <Home />
+        <Router>
+          <Sidebar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/profile" exact element={<Profile />} />
+            <Route path="/history" exact element={<History />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,20 +1,27 @@
 import "./Sidebar.css";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className="sidebar-base">
-      <div className="sidebar-content home-div">
-        <i className="bx bx-home-alt "></i>
-        <span className="home-sidebar-label sidebar-text">Home</span>
-      </div>
-      <div className="sidebar-content profile-div">
-        <i className="bx bx-user"></i>
-        <span className="profile-sidebar-label sidebar-text">Profile</span>
-      </div>
-      <div className="sidebar-content history-div">
-        <i className="bx bx-history"></i>
-        <span className="history-sidebar-label sidebar-text">History</span>
-      </div>
+      <Link to="/" className="home-link">
+        <div className="sidebar-content home-div">
+          <i className="bx bx-home-alt "></i>
+          <span className="home-sidebar-label sidebar-text">Home</span>
+        </div>
+      </Link>
+      <Link to="/profile" className="profile-link">
+        <div className="sidebar-content profile-div">
+          <i className="bx bx-user"></i>
+          <span className="profile-sidebar-label sidebar-text">Profile</span>
+        </div>
+      </Link>
+      <Link to="/history" className="history-link">
+        <div className="sidebar-content history-div">
+          <i className="bx bx-history"></i>
+          <span className="history-sidebar-label sidebar-text">History</span>
+        </div>
+      </Link>
       <div className="sidebar-content log-out-div">
         <i className="bx bx-log-out"></i>
         <span className="log-out-sidebar-label sidebar-text">Log out</span>
